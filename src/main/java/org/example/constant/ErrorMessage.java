46e3c9e94b39469b25d4193585a520368eee8a7a
thachpham2103 <phamthach2103@gmail.com>
2025-05-21 14:48:13 +0700

    Initial commit on users branch

---
+++ src/main/java/org/example/constant/ErrorMessage.java
@@ -0,0 +1,19 @@
+package org.example.constant;
+
+public class ErrorMessage {
+
+    public class Category{
+        public static final String ERR_CREATE_CATEGORY = "Lỗi thêm danh mục";
+        public static final String ERR_UPDATE_CATEGORY = "Lỗi cập nhật danh mục";
+        public static final String ERR_GET_BY_ID_CATEGORY = "Lỗi lấy danh mục theo id = %d";
+        public static final String ERR_NOT_FOUND = "Lỗi lấy danh mục";
+    }
+
+    public class Product{
+        public static final String ERR_CREATE_PRODUCT = "Lỗi thêm sản phẩm";
+        public static final String ERR_UPDATE_PRODUCT = "Lỗi cập nhật sản phẩm";
+        public static final String ERR_GET_BY_ID_PRODUCT = "Lỗi lấy sản phẩm theo id = %d";
+        public static final String ERR_NOT_FOUND = "Lỗi lấy sản phẩm";
+    }
+
+}
